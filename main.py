9c5e93c1479675c89629a97ed06c04c69f0a64dd
Deepak M S <deepakcoder80@gmail.com>
2023-03-30 14:18:34 +0530

    student validation

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from server import models
 from server.database import engine
-from authenticationRoutes import adminRoutes
+from authenticationRoutes import adminRoutes, studentRoutes
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(adminRoutes.adminRouter)
-
+app.include_router(studentRoutes.studentRouter)
 
 @app.get("/")
 def read_root():
--- server/models.py
+++ server/models.py
@@ -10,3 +10,11 @@ class Admin(Base):
     email = Column(String, unique=True)
     password = Column(String)
     college = Column(String)
+
+
+class Student(Base):
+    __tablename__ = "student"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    password = Column(String)
+    college = Column(String)
