22f81a950b6d98c6082bc2c95de8a066d5ea4fe3
Hridya T N <tnhridya01@gmail.com>
2023-03-30 09:17:55 +0530

    qn model updated

--- routes/qnpaper_routes.py
+++ routes/qnpaper_routes.py
@@ -4,3 +4,8 @@ from server.database import engine
 
 qnPaperRouter = APIRouter()
 models.Base.metadata.create_all(bind=engine)
+
+
+# route for getting qnpaper by subject
+
+# route for getting qnpaper by semester
